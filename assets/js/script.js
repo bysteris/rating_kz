35c1e16b0b3ff0ad3597d5c1457afdd117ace4cc
bysteris <sb.steris@gmail.com>
2023-04-24 10:47:15 +0500

    адаптивы

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,32 @@ $(document).ready(function () {
         slideToScroll: 1,
         dots: false,
         arrows: true,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
+
+    let burgerBtn = document.querySelector(".burger-btn"),
+        menu = document.querySelector(".menu"),
+        menuMob = document.querySelector(".menu-mob");
+
+    burgerBtn.addEventListener("click", () => {
+        burgerBtn.classList.toggle("active");
+        setTimeout(() => menu.classList.toggle("active"), 100);
+        menuMob.classList.toggle("active");
+    });
+
 });
